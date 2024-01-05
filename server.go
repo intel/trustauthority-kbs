1ef688c967aad849a4adab1698325bc17187e686
hmgowda <harshitha.m.gowda@intel.com>
2024-01-05 10:10:03 -0800

    Upgrade TLS version to v1.3 (#65)

--- server.go
+++ server.go
@@ -148,11 +148,11 @@ func (app *App) startServer() error {
 	}
 	log.Debugf("Starting HTTPS server with TLS cert: %s", constant.DefaultTLSCertPath)
 	tlsConfig := &tls.Config{
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
+		MinVersion: tls.VersionTLS13,
+		CipherSuites: []uint16{tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_AES_128_GCM_SHA256,
+			// TLS_AES_128_CCM_SHA256 is not supported by go crypto/tls package
+			tls.TLS_CHACHA20_POLY1305_SHA256},
 	}
 	httpServer.TLSConfig = tlsConfig
 
