b9d617889098f60a1ffd1db7d7a6d5f29e1aaf8b
Patrick Colp <patrick.colp@oracle.com>
2024-08-08 15:27:19 -0700

    oci_key_manager: Add CreateKey() functionality
    Signed-off-by: Patrick Colp <patrick.colp@oracle.com>

--- keymanager/oci_key_manager.go
+++ keymanager/oci_key_manager.go
@@ -9,6 +9,11 @@ package keymanager
 import (
 	"intel/kbs/v1/model"
 	"intel/kbs/v1/ociclient"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 type OCIManager struct {
@@ -20,7 +25,38 @@ func NewOCIManager(c ociclient.OCIClient) *OCIManager {
 }
 
 func (om *OCIManager) CreateKey(keyRequest *model.KeyRequest) (*model.KeyAttributes, error) {
-	return nil, nil
+	if keyRequest.OciInfo.CompartmentId == "" || keyRequest.OciInfo.KeyId == "" ||
+		keyRequest.OciInfo.SecretName == "" || keyRequest.OciInfo.VaultId == "" {
+		return nil, errors.New("Missing oci_compartment_id, oci_key_id, oci_secret_name, or oci_vault_id")
+	}
+
+	newUuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new UUID")
+	}
+
+	keyAttributes := &model.KeyAttributes{
+		ID:               newUuid,
+		Algorithm:        keyRequest.KeyInfo.Algorithm,
+		KeyLength:        keyRequest.KeyInfo.KeyLength,
+		TransferPolicyId: keyRequest.TransferPolicyID,
+		CreatedAt:        time.Now().UTC(),
+		Oci: &model.OciAttributes{
+			CompartmentId: keyRequest.OciInfo.CompartmentId,
+			KeyId:         keyRequest.OciInfo.KeyId,
+			SecretName:    keyRequest.OciInfo.SecretName,
+			VaultId:       keyRequest.OciInfo.VaultId,
+		},
+	}
+
+	log.Infof("OCI: Creating key: algorithm = %q; secret name = %q", keyAttributes.Algorithm, keyAttributes.Oci.SecretName)
+
+	keyAttributes.Oci.SecretId, err = om.client.CreateKey(keyAttributes.Oci.CompartmentId, keyAttributes.Oci.KeyId, keyAttributes.Oci.SecretName, keyAttributes.Oci.VaultId)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create key")
+	}
+
+	return keyAttributes, nil
 }
 
 func (om *OCIManager) DeleteKey(keyAttributes *model.KeyAttributes) error {
